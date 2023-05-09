8e069fdb55096809cbca9b56b337b6041de47121
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-09 11:49:40 +0400

    Create Online_Banking_Application and its Packages

---
+++ src/test/java/com/Online_Banking_Application/Online_Banking_Application/OnlineBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Online_Banking_Application.Online_Banking_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
